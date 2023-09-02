bd519eedf447f72f878714d4cec77b14803b7f3c
tahmidachoudhury <tahmidachoudhury@outlook.com>
2023-09-02 21:34:28 +0100

    grid size can react to input 'clear board' feature has been added - yet to be tested

--- etch.js
+++ etch.js
@@ -1,9 +1,11 @@
+const content = document.querySelector('.content')
 const grid = document.querySelector('.grid');
 const inputSize = document.getElementById('numOfBoxes');
 const displaySize = document.querySelector('.grid-size');
 const displaygridSize = document.createElement('p');
-displaySize.appendChild(displaygridSize)
+displaySize.appendChild(displaygridSize);
 const defaultSquares = 8;
+displaygridSize.textContent = `${defaultSquares}x${defaultSquares}`;
 
 
 
@@ -14,10 +16,11 @@ function getSquareSize(sqrs){
     return squareSize
 }
 
+
 function buildGrid(squares){
     for (let row = 0; row < squares; row++) {
         for (let col = 0; col < squares; col++) {
-            const gridElement = document.createElement('div');
+            gridElement = document.createElement('div');
             gridElement.classList.add('grid-squares');
             gridElement.style.height = `${getSquareSize(squares)}px`;
             gridElement.style.width = `${getSquareSize(squares)}px`;
@@ -26,14 +29,43 @@ function buildGrid(squares){
     }    
 }
 
+//initial grid size from when the page refreshes
+buildGrid(defaultSquares);
+
+
+
+function removeGrid(){
+    const allGridSquares = document.querySelectorAll('.grid-squares');
+    const gridSquaresArray = Array.from(allGridSquares);
+    gridSquaresArray.forEach(element => {
+        grid.removeChild(element);
+    });
+}
+
+
+
+//changes grid size to input---------------------------
 inputSquareSize = function(e){
     sqrSize = e.target.value;
     displaygridSize.textContent = `${sqrSize}x${sqrSize}`;
+    removeGrid();
+    buildGrid(Number(inputSize.value));
 }
-
 inputSize.addEventListener('input', inputSquareSize);
+//-----------------------------------------------------
 
-buildGrid(defaultSquares);
+
+
+
+//clears screen --------------------------------------
+const clearScreen = function(e){
+    removeGrid();
+    buildGrid(Number(inputSize.value));
+}
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clearScreen);
+//-----------------------------------------------------
+
 
 
 
